33bcdb3918a6d55ba9b13de9449b05c6ab681d41
adrianrdguez <adrian.rodriguez@singularfactory.com>
2023-05-16 13:25:31 +0100

    Basic User Table

---
+++ apps/frontend/src/components/UserTable.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useTable, Column } from 'react-table';
+
+interface TableData {
+  id: number;
+  name: string;
+  age: number;
+}
+
+const Table = () => {
+  // Sample data
+  const data: TableData[] = React.useMemo(
+    () => [
+      { id: 1, name: 'John', age: 25 },
+      { id: 2, name: 'Jane', age: 30 },
+      { id: 3, name: 'Bob', age: 35 },
+    ],
+    []
+  );
+
+  // Column definitions
+  const columns: Column<TableData>[] = React.useMemo(
+    () => [
+      { Header: 'ID', accessor: 'id' },
+      { Header: 'Name', accessor: 'name' },
+      { Header: 'Age', accessor: 'age' },
+    ],
+    []
+  );
+
+  // Use the useTable hook to create table instance
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+  });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
--- apps/frontend/src/pages/Users.tsx
+++ apps/frontend/src/pages/Users.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import Table from '../components/UserTable';
 
-const Users = () =>
-  <div>Page for the Users</div>
+const Users = () => (
+  <>
+    <Table />
+  </>
+)
 
 export default Users;
